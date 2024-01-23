2b1718464fae92beea03d1325a2fa95b06ef96a7
vankerok <100090568+vankerok@users.noreply.github.com>
2024-01-22 20:32:35 -0700

    Update config.py
    Making comments sound clear

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # -*- coding: utf8 -*-
 
 from_channel = {
-    1111111111: '#examplechannel',    #channel_id: "parsing_name"
+    1111111111: '#examplechannel',            # forward from channel ( id ): "name that will be used during parsing"
 }
 
-to_channel = 1111111111    #channel_id
-forum_topic = 1111    #topic_id - only if you want to parse to forum thread, otherwise just delete this line
+to_channel = 1111111111                       # mirror to, channel or forum ( id )
+forum_topic = 1111                            # mirror to specific topic ( id ) - only if you want to parse to forum thread, otherwise just delete this line
 
 tg_user = {
-    'name': '',    #api name        https://my.telegram.org/
-    'api_id': '',    #api id
-    'api_hash': ''    #api hash
+    'name': '',                               # api name        https://my.telegram.org/
+    'api_id': '',                             # api id          https://my.telegram.org/
+    'api_hash': ''                            # api hash        https://my.telegram.org/
 }
